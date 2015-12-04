aac1364197607e97d7f366959bf78bc7e09fe8c9
Briehan Lombaard <briehan@briehansair.cape.saao.ac.za>
2015-12-04 11:03:57 +0200

    Adding gitconfig.

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s "${PWD}/bash_aliases" ~/.bash_aliases
 ln -s "${PWD}/screenrc" ~/.screenrc
 ln -s "${PWD}/vim" ~/.vim
 ln -s "${PWD}/vim/vimrc" ~/.vimrc
+ln -s "${PWD}/gitconfig" ~/.gitconfig
 
 git submodule init
 git submodule update
