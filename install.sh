2e5792a0cc407f110482bac072b10fc4242a9c44
Briehan Lombaard <briehan.lombaard@gmail.com>
2015-12-04 11:35:32 +0200

    Use $HOME instead of ~/

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 cd "${0%/*}"
 
-ln -s "${PWD}/bash_aliases" ~/.bash_aliases
-ln -s "${PWD}/screenrc" ~/.screenrc
-ln -s "${PWD}/vim" ~/.vim
-ln -s "${PWD}/vim/vimrc" ~/.vimrc
-ln -s "${PWD}/gitconfig" ~/.gitconfig
+ln -s "${PWD}/bash_aliases" "${HOME}/.bash_aliases"
+ln -s "${PWD}/screenrc" "${HOME}/.screenrc"
+ln -s "${PWD}/vim" "${HOME}/.vim"
+ln -s "${PWD}/vim/vimrc" "${HOME}/.vimrc"
+ln -s "${PWD}/gitconfig" "${HOME}/.gitconfig"
 
 git submodule init
 git submodule update
