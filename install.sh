6cecb503ea0b0c728157627a12589068c2141f95
Briehan Lombaard <briehan.lombaard@gmail.com>
2016-03-10 16:02:01 +0200

    Adding sqliterc

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s "${PWD}/bash_aliases" "${HOME}/.bash_aliases"
 ln -s "${PWD}/screenrc" "${HOME}/.screenrc"
 ln -s "${PWD}/vim" "${HOME}/.vim"
 ln -s "${PWD}/vim/vimrc" "${HOME}/.vimrc"
+ln -s "${PWD}/sqliterc" "${HOME}/.sqliterc"
 ln -s "${PWD}/gitconfig" "${HOME}/.gitconfig"
 
 git submodule init
