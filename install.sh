161536c9aefb1f4592016ba699f43f78e9350224
Briehan Lombaard <briehan.lombaard@gmail.com>
2018-07-11 15:58:25 +0200

    Link bin to ~/bin when installing

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -s "${PWD}/vim/vimrc" "${HOME}/.vimrc"
 ln -s "${PWD}/sqliterc" "${HOME}/.sqliterc"
 ln -s "${PWD}/gitconfig" "${HOME}/.gitconfig"
 ln -s "${PWD}/gitignore" "${HOME}/.gitignore"
+ln -s "${PWD}/bin" "${HOME}/bin"
 
 git submodule init
 git submodule update
